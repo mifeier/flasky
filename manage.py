debeade4f52fdc1e5a5058fc2d57b6765ea4de2a
wangtian <wangtian@ygsoft.com>
2018-06-04 10:38:20 +0800

    yidui

---
+++ manage.py
@@ -0,0 +1,35 @@
+import os
+from app import create_app,db
+from app.models import User,Role
+from flask_script import Manager,Shell
+from flask_migrate import Migrate,MigrateCommand
+from flask_login import login_required
+
+app=create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager=Manager(app)
+migrate=Migrate(app,db)
+
+def make_shell_context():
+    return dict(app=app,db=db,User=User,Role=Role)
+manager.add_command('shell',Shell(make_context=make_shell_context))
+manager.add_command('db',MigrateCommand)
+
+@manager.command
+def test():
+    """Run the unit tests"""
+    import unittest
+    tests=unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+# @app.route('/')
+# def index():
+#     return '<h1>Hello World!</h1>'
+
+
+# @app.route('/secret')
+# @login_required
+# def secret():
+#     return 'Only authenticated users are allowed!'
+
+if __name__=='__main__':
+    manager.run()
