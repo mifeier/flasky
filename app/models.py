21deecc732ae9dacad9fdbe78eb8836b88310832
wangtian <1115449386@qq.com>
2018-06-06 18:08:49 +0800

    xindiannaoqueren

--- app/models.py
+++ app/models.py
@@ -41,7 +41,7 @@ class User(UserMixin,db.Model):
 
     @property
     def password(self):
-        raise AttributeError('password is not a readable attribute')
+        raise AttributeError('the password is not a readable attribute')
     @password.setter
     def password(self,password):
         self.password_hash=generate_password_hash(password)
